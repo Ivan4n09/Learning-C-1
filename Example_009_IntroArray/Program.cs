871864ff38e5088d81db4ca6baabeb5995b48ede
Ivan <iss04@yandex.ru>
2023-04-13 04:37:11 +0300

    Добавил Example_009 - 011

---
+++ Example_009_IntroArray/Program.cs
@@ -0,0 +1,20 @@
+﻿int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if (arg2 > result) result = arg2;
+    if (arg3 > result) result = arg3;
+    return result;
+}//инд массива  0   1   2   3   4   5   6   7   8
+
+int[] array = {15, 29, 38, 46, 54, 63, 97, 82, 29};
+
+// примеры команд
+//array[0] = 12;
+// Console.WriteLine(array[4]);
+
+int result = Max(
+    Max(array[0], array[1], array[2]),
+    Max(array[3], array[4], array[5]),
+    Max(array[6], array[7], array[8])
+);
+Console.WriteLine(result);
---
+++ Example_011_ArrayLibrary/Program.cs
@@ -0,0 +1,51 @@
+﻿void FillArray(int[] collection)
+
+{
+    int length = collection.Length;
+    int index =0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(1, 10);
+        index++;
+    }
+}
+
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+    }
+}
+
+int IndexOf(int[] collection, int find)
+{
+    int count = collection.Length;
+    int index = 0;
+    int position = -1; // если искомого индекса указанного элемента нету - то он покажет -1
+    while (index<count)
+    {
+        if(collection[index] == find)
+        {
+            position = index;
+            break;
+        }
+        index++;
+    }
+    return position;
+}
+
+int[] array = new int[10];
+
+FillArray(array);
+array[4] = 4;
+array[6] = 4;
+PrintArray(array);
+Console.WriteLine();
+
+int pos = IndexOf(array, 4);
+// или int pos = IndexOf(array, 42); - чтобы посмотреть на -1
+Console.WriteLine(pos);
