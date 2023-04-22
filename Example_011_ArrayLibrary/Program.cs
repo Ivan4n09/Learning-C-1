26c95f891d3b6a451b8873a37c82b10e2b209455
Ivan <iss04@yandex.ru>
2023-04-22 03:06:08 +0300

    Added Example_012 (large)

--- Example_011_ArrayLibrary/Program.cs
+++ Example_011_ArrayLibrary/Program.cs
@@ -2,7 +2,7 @@
 
 {
     int length = collection.Length;
-    int index =0;
+    int index = 0;
     while (index < length)
     {
         collection[index] = new Random().Next(1, 10);
@@ -26,9 +26,9 @@ int IndexOf(int[] collection, int find)
     int count = collection.Length;
     int index = 0;
     int position = -1; // если искомого индекса указанного элемента нету - то он покажет -1
-    while (index<count)
+    while (index < count)
     {
-        if(collection[index] == find)
+        if (collection[index] == find)
         {
             position = index;
             break;
